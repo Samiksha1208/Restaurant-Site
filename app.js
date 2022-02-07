4954d115847ee2fec8eaa3e0418364ce62ece9aa
Samiksha1208 <samikshajadhav1208@gmail.com>
2022-02-07 14:33:06 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+var menu1 = document.getElementById("menu1");
+var menu2 = document.getElementById("menu2");
+var menu3 = document.getElementById("menu3");
+var menu4 = document.getElementById("menu4");
+var menu5 = document.getElementById("menu5");
+var menu6 = document.getElementById("menu6");
+var cont3 = document.getElementsByClassName("container3")[0]; 
+function OnAll(){
+        menu1.style.display = 'block';
+        menu2.style.display = 'block';
+        menu3.style.display = 'block';
+        menu4.style.display = 'block';
+        menu5.style.display = 'block';
+        menu6.style.display = 'block';
+        cont3.style.height = "auto";
+}
+function OnBreakFast(){
+        menu1.style.display = 'block';
+        menu2.style.display = 'none';
+        menu3.style.display = 'none';
+        menu4.style.display = 'block';
+        menu5.style.display = 'block';
+        menu6.style.display = 'block';
+        cont3.style.height = "auto";
+}
+function OnLunch(){
+        menu1.style.display = 'none';
+        menu2.style.display = 'none';
+        menu3.style.display = 'none';
+        menu4.style.display = 'none';
+        menu5.style.display = 'block';
+        menu6.style.display = 'block';
+        cont3.style.height = "auto";
+}
+function OnDinner(){
+        menu1.style.display = 'block';
+        menu2.style.display = 'block';
+        menu3.style.display = 'none';
+        menu4.style.display = 'none';
+        menu5.style.display = 'block';
+        menu6.style.display = 'none';
+        cont3.style.height = "auto";
+}
+function OnCoffee(){
+        menu1.style.display = 'none';
+        menu2.style.display = 'block';
+        menu3.style.display = 'none';
+        menu4.style.display = 'block';
+        menu5.style.display = 'block';
+        menu6.style.display = 'none';
+        cont3.style.height = "auto";
+}
+function OnSnacks(){
+        menu1.style.display = 'none';
+        menu2.style.display = 'none';
+        menu3.style.display = 'none';
+        menu4.style.display = 'block';
+        menu5.style.display = 'block';
+        menu6.style.display = 'block';
+        cont3.style.height = "auto";
+}
